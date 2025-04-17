6e59ce20a257491157272a6cf6c718dd95b23bb0
rac328 <raul.cabrera@edu.uah.es>
2025-04-17 15:13:49 +0000

    commit inicial

---
+++ main/java/org/example/Comedor.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.lang.Thread.sleep;
+
+public class Comedor {
+    private AtomicInteger comidaDisponible = new AtomicInteger(0);
+    private Semaphore comer;
+    private ReentrantLock comidaEsperar = new ReentrantLock();
+    private Condition noComida = comidaEsperar.newCondition();
+
+    public Comedor(){
+    }
+
+    public void depositarComida(Humano hu){
+        if(hu.llevaComida()){
+            comidaDisponible.incrementAndGet();
+            hu.setComida(false);
+            comidaEsperar.lock();
+            noComida.signal();
+            comidaEsperar.unlock();
+        }
+    }
+
+    public void comer(){
+            try {
+                comidaEsperar.lock();
+                while(comidaDisponible.get() == 0){
+                    noComida.await();
+                }
+                comidaDisponible.decrementAndGet();
+                comidaEsperar.unlock();
+                sleep((long) (Math.random()*3000+5000));
+
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+    }
+}
---
+++ main/java/org/example/Zombie.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Zombie {
+    int contadormuertes;
+    public Zombie(){
+
+    }
+}
